4a769b309e881323281cc75aeb25672889ea1972
Naveen-2007 <naveenkumar78987@gmail.com>
2026-05-06 17:43:23 +0530

    Remove unused plotly import fixing cloud deploy

--- scripts/train_ipl_2026_current.py
+++ scripts/train_ipl_2026_current.py
@@ -13,8 +13,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.preprocessing import LabelEncoder
-import plotly.express as px
-import plotly.graph_objects as go
+
 
 warnings.filterwarnings('ignore')
 
